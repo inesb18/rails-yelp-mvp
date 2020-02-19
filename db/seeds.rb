43e732f4322f9f94beb64ec86931a95b105e0b89
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-19 12:36:09 -0500

    Created the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+categories = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+20.times do
+  new_restaurant = Restaurant.new(name: Faker::Restaurant.name)
+  new_restaurant.category = categories.sample
+  new_restaurant.address = Faker::Address.full_address
+  if rand > 0.5
+    new_restaurant.phone_number = Faker::PhoneNumber.phone_number_with_country_code
+  end
+  new_restaurant.save
+end
