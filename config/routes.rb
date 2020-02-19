dad40320e6cfb9ed6c665e036c1ebc3e928bf480
Ines Belghiti <ines.belghiti@gmail.com>
2020-02-19 15:57:16 -0500

    Embedded the review form inside each restaurant's show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :new, :create, :show]
-  resources :restaurants do
-    resources :reviews, only: [:new, :create]
+  resources :restaurants, only: [:show] do
+    resources :reviews, only: [:create]
   end
 end
